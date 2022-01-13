d04258ebfe13a38fbc0eabefcd66f95fe033d773
MHutten <milanhutten@live.nl>
2022-01-13 22:23:14 +0100

    removed build folder

--- cmake-build-release/CMakeFiles/3.21.1/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/Users/s164009/Downloads/winlibs-x86_64-posix-seh-gcc-11.2.0-llvm-13.0.0-mingw-w64ucrt-9.0.0-r2/mingw64/bin/windres.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .obj)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
--- cmake-build-release/unit_testing/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/s164009/Documents/GitHub/ParityGameSolver/unit_testing/lib/googletest
-# Build directory: C:/Users/s164009/Documents/GitHub/ParityGameSolver/cmake-build-release/unit_testing/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
